e91a5bc9dc316a51040f1178151817c9253d91e9
ycao123 <96071865+ycao123@users.noreply.github.com>
2022-01-10 14:02:57 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 # server.py
-# LOOK AT IFCONFIG EN0 BROADCAST
 import socket, sys, time, random, pickle
 import functions
 
@@ -12,8 +11,7 @@ tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Creates the socket tuple
 self_ip = "10.153.61.239"
-#self_port = random.randint(49152,65535)
-self_port = 8080
+self_port = 8000
 print("Port: ", self_port)
 self_tcp = (self_ip, self_port)
 
@@ -25,60 +23,16 @@ print("TCP socket: ", self_tcp)
 print("FOR TEST. TRY BOTH IPS")
 functions.get_gateway_ip()
 
-# Generates the key for encryption/decryption
-# key = functions.generate_key()
-
-# Listens for any incoming connections
-tcp_socket.listen(1)
 print("\nWaiting for connection...\n")
 
-
+# Listens for any incoming connections
+tcp_socket.listen()
+    
 conn, addr = tcp_socket.accept()
 
 with conn:
     print("Connected by ", addr)
+    recv = functions.recv(conn)
+    functions.send(conn, b"END")
 
-def receive_message():
-    message = pickle.loads(conn.recv(4096))
-    message_type = type(message)
-    if isinstance(message_type, type(list)):
-        time.sleep(1)
-        print("Received Data!")
-        print(message)
-        return list(message)
-    else:
-        time.sleep(1)
-        return list("ERROR")
-
-def send_text(text, ip):
-    data = pickle.dumps(text)
-    tcp_socket.sendto(data, ip)
-
-def send_file(filename, ip):
-    with open(file, "r"):
-        data = file.read()
-    send_text(data, ip)
-
-# Repeat forever or until client sends [e]
-while True:
-    data = receive_message()
-    if data[1] == "CONNECT":
-        client_ip = (data[0], 8080)
-        clients.append(client_ip)
-        message = data[1]
-        print(f"\nReceived connection request from {client_ip}")
-        print(f"Accepted connection request from {client_ip}\n")
-        print(f"Sending CONNECTION_APPROVED\n\n")
-        send_text(["SERVER", "CONNECTION_APPROVED"], client_ip)
-    elif data[1] == "":
-        message = input(str("Me : "))
-        if message == "[e]":
-            message = "Client left"
-            conn.send(message.encode())
-            print("\n")
-            break
-    else:
-        udp_socket.sendto(message, client_ip)
-        message = s.recv(1024)
-        message = message.decode()
-        print("Client: ", message)
+tcp_socket.close()
