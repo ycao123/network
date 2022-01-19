2cdafcc7bcb1529d1f23cedbaa0e677a4a3879fc
ycao123 <96071865+ycao123@users.noreply.github.com>
2022-01-19 09:38:57 -0700

    Update server.py

--- server.py
+++ server.py
@@ -10,14 +10,15 @@ print("Initialising....\n")
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Creates the socket tuple
-self_ip = "10.153.61.239"
+self_ip = socket.gethostname()
 self_port = 8000
 print("Port: ", self_port)
 self_tcp = (self_ip, self_port)
 
 # Binds to the host and port
 tcp_socket.bind(self_tcp)
-print("TCP socket: ", self_tcp)
+print("\nIP address: ", self_ip)
+print("\nTCP socket: ", self_tcp)
 
 # Prints the gateway/public IPv4
 print("FOR TEST. TRY BOTH IPS")
