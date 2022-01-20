26a943643f641375815b3a0cd74ca1c56bf39576
ycao123 <96071865+ycao123@users.noreply.github.com>
2022-01-20 11:15:55 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -9,9 +9,6 @@ try:
     import netifaces
 except ImportError:
     print("Missing these modules:\nnetifaces")
-#from cryptography.fernet import Fernet
-
-default_key = b'D_nB_gRXA6AjnbHDuIYyj74XFx8X8YlVWns1_4qF_sY='
 
 # Gets the gateway/public IPv4
 def get_gateway_ip():
@@ -23,6 +20,7 @@ def get_gateway_ip():
 
 # Gets local IPv4
 def get_ip():
+    "Gets the local IPv4 of the "
     ip = socket.gethostbyname(socket.gethostname())
     print("IPv4: ", ip)
     return ip
@@ -36,38 +34,3 @@ def recv(tcp_socket):
 def send(tcp_socket, text):
     data = pickle.dumps(text)
     tcp_socket.send(data)
-'''   
-# Encrypts a message
-def encrypt(text, key):
-    fernet = Fernet(key)
-    message = fernet.encrypt(pickle.dumps(text))
-    return message
-
-# Decrypts a message
-def decrypt(text, key):
-    fernet = Fernet(key)
-    message = pickle.loads(fernet.decrypt(text))
-    return message
-
-# Generates a key to use for encryption/decryption
-def generate_key():
-    answer = input("Generate key (default is no)? [yes/no] ")
-    if answer == "yes":
-        key = Fernet.generate_key().decode()
-        print("Key: ", key)
-    else:
-        key = default_key
-    return key
-
-# Loads a key for encryption/decryption
-def load_key():
-    answer = input("Load key (default is no)? [yes/no] ")
-    if answer == "yes":
-        key = input("Key: ").encode()
-        if len(key) != 44:
-            print("Invalid Key")
-            sys.exit(2)
-    else:
-        key = default_key
-    return key
-'''
