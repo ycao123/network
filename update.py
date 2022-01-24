e460455a3687574a315f318471b31723a41f457f
ycao123 <96071865+ycao123@users.noreply.github.com>
2022-01-24 13:07:01 -0700

    Update update.py

--- update.py
+++ update.py
@@ -1,1 +1,29 @@
+'''
+Update function for the program
+Usage:
+    Just make changes
+    PUSH THEM TO GITHUB
+    To update, call python3 update.py
+'''
 
+# Imports
+from os import getcwd, chdir
+from subprocess import check_output, CalledProcessError
+
+def main():
+    directory = getcwd()
+    parent_directory = directory.replace("/network", "")
+    chdir(parent_directory)
+    try:
+        #delete_directory = check_output(["rm", "-R", "network"])
+        #git_clone = check_output(["git", "clone", "https://github.com/ycao123/network"])
+        
+    except CalledProcessError:
+        print("Error: Bad Stuff Detected")
+        exit(1)
+
+    #git_clone = git_clone.decode()
+    #print(git_clone)
+
+if __name__ == "__main__":
+    main()
